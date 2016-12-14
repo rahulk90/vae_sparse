3b5905ac512a9dffba9fba6c4bc4bfb3cce3ea7b
Rahul Krishnan <rahulk90@gmail.com>
2016-12-14 18:05:22 -0500

    Added compile timing + getTest result

---
+++ expt/compileTiming.py
@@ -0,0 +1,19 @@
+import glob,os
+import numpy as np
+from utils.misc import loadHDF5,getConfigFile,readPickle
+for f in glob.glob('./chkpt-*/*-EP400-stats.h5'):
+    code = 'ds'+os.path.basename(f).split('-ql')[0].split('ds')[1]
+    if 'finopt' in f:
+        code = 'finopt-'+code
+    else:
+        code = 'none-'+code
+    data = loadHDF5(f)
+    params=readPickle(getConfigFile(f))[0]
+    code = params['dataset']+'-'+code
+    runtimes = [] 
+    for edata in data['batch_time']:
+        if int(edata[0])%params['savefreq']==0:
+            continue
+        else:
+            runtimes.append(edata[1])
+    print code, np.mean(runtimes)
---
+++ expt/getTestResults.py
@@ -0,0 +1,20 @@
+import glob
+from utils.misc import loadHDF5,getConfigFile, readPickle
+#DIR = './results-sept6/'
+DIR = './'
+
+dataset = '20newsgroups_miao'
+dataset = 'rcv2_miao'
+
+for f in glob.glob(DIR+'/chkpt-'+dataset+'_*/*evaluate.h5'):
+    if 'mnist' in f:
+        continue
+    dataset = f.split('chkpt-')[1].split('-')[0]
+    opt_type= f.split('chkpt-')[1].split('-')[1].split('/')[0]
+    params  = readPickle(getConfigFile(f.replace('evaluate.h5','')))[0] 
+    dset    = loadHDF5(f)
+    if params['opt_type']=='finopt':
+        name = str(params['p_layers'])+'-M'+str(params['n_steps'])+'-'+params['input_type']
+    else:
+        name = str(params['p_layers'])+'-M1-'+params['input_type']
+    print params['dataset'],name, dset['perp_0_eb'],dset['perp_f_eb']
