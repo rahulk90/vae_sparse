29247ad007ce2a44f08a725c31c24350d2c1aee5
Rahul G. Krishnan <rahulk90@gmail.com>
2017-01-09 18:38:56 -0500

    Added gitignore + vizresults setup

--- optvaeutils/viz.py
+++ optvaeutils/viz.py
@@ -23,9 +23,18 @@ def getName(params):
         name = 'fin_mult'+str(float(params['p_updates']))+'_'+str(int(params['n_steps']/float(params['p_updates'])))
     else:
         assert False,'Invalid choice for otype:'+str(params['opt_type'])
-
-    if params['projected_gradient']:
-        name += '_pg'
-    if params['batch_normalize']:
-        name += '_bn'
+    #if params['batch_normalize']:
+    #    name += '_bn'
     return name
+
+def processTrainValidBounds(train_bound, valid_bound, savefreq):
+    """ savefreq: freq of saving validation bounds 
+        train/valid_bound : 2D mat (epoch - value)
+    """
+    assert train_bound.ndim==2 and valid_bound.ndim==2,'Expecting 2D arrays'
+    new_bd      = np.zeros_like(valid_bound)
+    idx         = np.arange(0,train_bound[-1,0].astype(int)+1,savefreq)
+    new_bd[:,0] = idx
+    new_bd[:,1] = train_bound[idx.ravel().astype(int),1]
+    new_bd      = new_bd[~np.isnan(new_bd).any(axis=1)]
+    return new_bd
