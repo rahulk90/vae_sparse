46e32f61c93d517ee785527419d17fd163937d06
Rahul G. Krishnan <rahulk90@gmail.com>
2017-01-26 13:05:35 -0500

    updated sentiment

--- optvaedatasets/sentiment.py
+++ optvaedatasets/sentiment.py
@@ -1,6 +1,6 @@
 from utils.misc import savePickle,downloadData,extractData
 import os,re
-from utils.misc import readPickle, savePickle
+from utils.misc import readPickle, savePickle, saveHDF5, loadHDF5
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -16,8 +16,8 @@ def setupDataset(dataset):
     for idx, elem in enumerate(dataset): 
         indices[idx,:len(elem)] = np.sort(np.array(elem)) 
         mask[idx,:len(elem)]    = 1. 
-    import ipdb;ipdb.set_trace()
     return indices, mask
+
 """
 Setup Stanford Sentiment Analysis Dataset
 """
@@ -31,7 +31,11 @@ def _processStanford(DIR, dset):
     if not os.path.exists(DIR+'/train-rootfine'):
         raise ValueError('Run the code sentiment/sentiment_trees.py first')
     elif os.path.exists(DIR+'/'+dset+'.pkl'):
-        return readPickle(DIR+'/'+dset+'.pkl')[0]
+        vocabdict = readPickle(DIR+'/'+dset+'.pkl')[0]
+        dataset   = loadHDF5(DIR+'/'+dset+'.h5')
+        for w in vocabdict:
+            dataset[w] = vocabdict[w]
+        return dataset
     else:
         assert dset in ['sst_fine','sst_binary'],'Invalid value for dset'
         train= readPickle(DIR+'/train-rootfine')[0]
@@ -55,15 +59,20 @@ def _processStanford(DIR, dset):
         dataset_fine['train_x'], dataset_fine['train_y'] = split(train) 
         dataset_fine['valid_x'], dataset_fine['valid_y'] = split(valid) 
         dataset_fine['test_x'],  dataset_fine['test_y']  = split(test) 
-
         dataset_fine['train_x'], dataset_fine['mask_train']   = setupDataset(dataset_fine['train_x'])
         dataset_fine['valid_x'], dataset_fine['mask_valid']   = setupDataset(dataset_fine['valid_x'])
         dataset_fine['test_x'],  dataset_fine['mask_test']    = setupDataset(dataset_fine['test_x'])
+        print 'sst_fine train:' , dataset_fine['train_x'].shape, dataset_fine['train_y'].shape, dataset_fine['mask_train'].shape
+        print 'sst_fine valid:' , dataset_fine['valid_x'].shape, dataset_fine['valid_y'].shape, dataset_fine['mask_valid'].shape
+        print 'sst_fine test :' , dataset_fine['test_x'].shape,  dataset_fine['test_y'].shape,  dataset_fine['mask_test'].shape
+        saveHDF5(DIR+'/sst_fine.h5', dataset_fine)
+        dataset_fine = {}
         dataset_fine['word2idx'] = word2idx
         idx2word = {}
         for w in word2idx:
             idx2word[word2idx[w]] = w
         dataset_fine['idx2word'] = idx2word
+        savePickle([dataset_fine],DIR+'/sst_fine.pkl')
 
         dataset_binary = {} 
         dataset_binary['train_x'], dataset_binary['train_y'] = split(train, binary=True) 
@@ -72,9 +81,13 @@ def _processStanford(DIR, dset):
         dataset_binary['train_x'], dataset_binary['mask_train']   = setupDataset(dataset_binary['train_x'])
         dataset_binary['valid_x'], dataset_binary['mask_valid']   = setupDataset(dataset_binary['valid_x'])
         dataset_binary['test_x'],  dataset_binary['mask_test']    = setupDataset(dataset_binary['test_x'])
+        print 'sst_bin train:' , dataset_binary['train_x'].shape, dataset_binary['train_y'].shape, dataset_binary['mask_train'].shape
+        print 'sst_bin valid:' , dataset_binary['valid_x'].shape, dataset_binary['valid_y'].shape, dataset_binary['mask_valid'].shape
+        print 'sst_bin test :' , dataset_binary['test_x'].shape,  dataset_binary['test_y'].shape,  dataset_binary['mask_test'].shape
+        saveHDF5(DIR+'/sst_binary.h5', dataset_binary)
+        dataset_binary  = {}
         dataset_binary['word2idx'] = word2idx
         dataset_binary['idx2word'] = idx2word
-        savePickle([dataset_fine],DIR+'/sst_fine.pkl')
         savePickle([dataset_binary],DIR+'/sst_binary.pkl')
         if dset =='sst_fine':
             return dataset_fine
@@ -103,7 +116,11 @@ def _processIMDB(DIR):
         raise ValueError('Run code in sentiment/preprocess_imdb.py')
     elif os.path.exists(DIR+'/imdb.pkl'):
         print 'Loading...'
-        return readPickle(DIR+'/imdb.pkl')[0]
+        dataset   = loadHDF5(DIR+'/imdb.h5')
+        vocabdict = readPickle(DIR+'/imdb.pkl')[0]
+        for k in vocabdict:
+            dataset[k] = vocabdict[k]
+        return dataset
     else:
         saved     = readPickle(DIR+'/aclImdb/imdb_splits')[0]
         train_valid, test_tup, _, word2idx= saved[0], saved[1], saved[2], saved[3]
@@ -124,14 +141,19 @@ def _processIMDB(DIR):
         for w in word2idx:
             idx2word[word2idx[w]] = w
         dataset = {}
-        dataset['word2idx']=word2idx
-        dataset['idx2word']=idx2word
         dataset['train_x'], dataset['train_y'] = split(train_tup) 
         dataset['valid_x'], dataset['valid_y'] = split(valid_tup) 
         dataset['test_x'], dataset['test_y']   = split(test_tup) 
         dataset['train_x'], dataset['mask_train'] = setupDataset(dataset['train_x'])
         dataset['valid_x'], dataset['mask_valid'] = setupDataset(dataset['valid_x'])
         dataset['test_x'], dataset['mask_test']   = setupDataset(dataset['test_x'])
+        print 'IMDB train:' , dataset['train_x'].shape, dataset['train_y'].shape, dataset['mask_train'].shape
+        print 'IMDB valid:' , dataset['valid_x'].shape, dataset['valid_y'].shape, dataset['mask_valid'].shape
+        print 'IMDB test :' , dataset['test_x'].shape,  dataset['test_y'].shape,  dataset['mask_test'].shape
+        saveHDF5( DIR+'/imdb.h5', dataset)
+        dataset = {}
+        dataset['word2idx']=word2idx
+        dataset['idx2word']=idx2word
         savePickle([dataset],DIR+'/imdb.pkl')
         print 'Saved....'
         return dataset
@@ -168,21 +190,23 @@ def _setupRT(DIR):
     positive_vecs = ctvec.transform(positive)
     negative_vecs = ctvec.transform(negative)
     
-    import ipdb;ipdb.set_trace()
     data  =[]
     labels=[]
     for pvec in positive_vecs:
-        pos_list  = pvec.toarray().squeeze().tolist()
+        pos_list  = pvec.toarray().squeeze()
         idxlist   = []
-        import ipdb;ipdb.set_trace()
-        for idx, count in enumerate(pos_list):
+        non_zero_idx = np.where(pos_list>0.)[0]
+        for idx in non_zero_idx.tolist(): 
+            count   = pos_list[idx]
             idxlist += [idx]*count
         data.append(idxlist)
         labels.append(1)
     for nvec in negative_vecs:
         neg_list  = nvec.toarray().squeeze().tolist()
         idxlist   = []
-        for idx, count in enumerate(neg_list):
+        non_zero_idx = np.where(neg_list>0.)[0]
+        for idx in non_zero_idx.tolist(): 
+            count    = neg_list[idx]
             idxlist += [idx]*count
         data.append(idxlist)
         labels.append(0)
@@ -207,17 +231,26 @@ def _setupRT(DIR):
     dataset['train_y']    = labels[train_idx] 
     dataset['valid_y']    = labels[valid_idx] 
     dataset['test_y']     = labels[test_idx] 
+    print 'RT train:' , dataset['train_x'].shape, dataset['train_y'].shape, dataset['mask_train'].shape
+    print 'RT valid:' , dataset['valid_x'].shape, dataset['valid_y'].shape, dataset['mask_valid'].shape
+    print 'RT test :' , dataset['test_x'].shape,  dataset['test_y'].shape,  dataset['mask_test'].shape
+    saveHDF5( DIR+'/rt.h5', dataset)
+    dataset = {}
     dataset['idx2word'] = idx2word
     dataset['word2idx'] = word2idx
-    savePickle([dataset],DIR+'/dataset.pkl')
+    savePickle([dataset],DIR+'/rt.pkl')
 
 def _loadRT():
     DIR = os.path.dirname(os.path.realpath(__file__)).split('inference_introspection')[0]+'inference_introspection/optvaedatasets/sentiment/rotten_tomatoes'
     if not os.path.exists(DIR):
         os.system('mkdir -p '+DIR)
-    if not (os.path.exists(DIR+'/dataset.pkl')):
+    if not (os.path.exists(DIR+'/rt.pkl')):
         _setupRT(DIR)
-    return readPickle(DIR+'/dataset.pkl')[0]
+    vocabdict = readPickle(DIR+'/rt.pkl')[0]
+    dataset   = loadHDF5(DIR+'/rt.h5')
+    for k in vocabdict:
+        dataset[k] = vocabdict[k]
+    return dataset
 
 if __name__=='__main__':
     sst_fine= _loadStanford('sst_fine')
